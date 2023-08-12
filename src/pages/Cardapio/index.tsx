e200c4173c6fb2a4e696b8f3af052ee4c9db13d5
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2023-08-12 08:12:11 -0300

    Cria header da página com imagem e texto

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,11 +2,16 @@ import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
 
 export default function Cardapio() {
- return (
-    <main>
-        <nav className={styles.menu}>
-            <Logo />
-        </nav>
-    </main>
- )
+    return (
+        <main>
+            <nav className={styles.menu}>
+                <Logo />
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A cada do código e da massa
+                </div>
+            </header>
+        </main>
+    )
 }
