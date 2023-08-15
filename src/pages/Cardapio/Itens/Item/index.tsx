68cf721193a69f8c7c8cfd5c88d49984c4fbb3f1
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2023-08-14 22:52:04 -0300

    Cria componente para reaproveitar tags dos pratos

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,9 +1,10 @@
+import TagsPrato from 'components/TagsPrato';
 import styles from './Item.module.scss';
-import classNames from 'classnames';
 import { Prato } from 'types/Prato';
 
 export default function Item(props: Prato) {
-    const { title, description, category, size, serving, price, photo } = props;
+    const prato = props;
+    const { title, description, photo } = prato;
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
@@ -14,23 +15,7 @@ export default function Item(props: Prato) {
                     <h2>{title}</h2>
                     <p>{description}</p>
                 </div>
-                <div className={styles.item__tags}>
-                    <div className={classNames({
-                        [styles.item__tipo]: true,
-                        [styles[`item__tipo__${category.label.toLowerCase()}`]]: true
-                    })}>
-                        {category.label}
-                    </div>
-                    <div className={styles.item__porcao}>
-                        {size}g
-                    </div>
-                    <div className={styles.item__qtdpessoas}>
-                        Serve {serving} pessoa{serving === 1 ? '' : 's'}
-                    </div>
-                    <div className={styles.item__valor}>
-                        R$ {price.toFixed(2)}
-                    </div>
-                </div>
+                <TagsPrato {...prato} />
             </div>
         </div>
     );
