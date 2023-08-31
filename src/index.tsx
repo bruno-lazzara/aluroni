4bfe3876f36123a9b7c5521ce42a6d99d8a4f887
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2023-08-30 23:46:08 -0300

    Remove código não utilizado

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Inicio from 'pages/Inicio';
 import { createRoot } from 'react-dom/client';
 import Router from 'routes';
-
-const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />;
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
